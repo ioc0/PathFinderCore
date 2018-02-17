5291b6e768848c7c714b30ce88d59fb1f2102374
Unknown <ioc0kernel@gmail.com>
2018-02-17 15:02:16 +0300

    Added RollADie method
    RaceModifier

--- PathFinderCore/PlayerTemplate.cs
+++ PathFinderCore/PlayerTemplate.cs
@@ -4,26 +4,54 @@ namespace PathFinderCore
 {
     public class PlayerTemplate
     {
+        //base Vars
         public string PlayerName;
         public string PlayerClass;
+        //return Race
+        public static string PlayerRace(int n){
+            if (n == 1)
+            {
+                return "Human";
+            }
+            else if (n == 2)
+            {
+                return "Dwarf";
+            }
+            else if (n == 3){
+                return "Elf";
+            }
+            else if (n == 4){
+                return "High Elf";
+            }
+            else return "Не правильный ввод";
+
+        }
+            
+
+            
+
         public int str;
         public int dex;
         public int con;
         public int pInt;
         public int wiz;
         public int pChar;
+        public int HP;
+        public int AC;
+
+
 
         //empty constructor;
 
-        public PlayerTemplate(){
+        public PlayerTemplate(int v)
+        {
             
         }
         //base characteristics;
 
-        public PlayerTemplate(string playerName, string playerClass, int str, int dex, int con, int pInt, int wiz, int pChar)
+        public PlayerTemplate(string playerName, int str, int dex, int con, int pInt, int wiz, int pChar)
         {
             PlayerName = playerName;
-            PlayerClass = playerClass;
             this.str = str;
             this.dex = dex;
             this.con = con;
--- PathFinderCore/Program.cs
+++ PathFinderCore/Program.cs
@@ -6,8 +6,23 @@ namespace PathFinderCore
     {
         static void Main(string[] args)
         {
-            PlayerTemplate newPlayer1 = new PlayerTemplate("dummy","someClass",12,12,12,13,14,15);
-            for (int i = 0; i < 100;i++){
+            int Race = 0;
+            Console.WriteLine("Введите Имя:");
+            var Name = Console.ReadLine();
+            Console.WriteLine("Введите расу: 1. Human, 2. Elf, 3. Dwarf, 4. High Elf");
+            try
+            {
+                Race = Int16.Parse(Console.ReadLine());
+            
+                }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Повторите ввод");
+            }
+            Console.WriteLine("Ввод расы: "+ Race);                       
+            PlayerTemplate newPlayer1 = new PlayerTemplate(Name,12,12,12,13,14,15);
+            for (int i = 0; i < 2;i++){
                 DiceRoll dr = new DiceRoll();
 
 
@@ -15,6 +30,7 @@ namespace PathFinderCore
             }
 
             Console.WriteLine("Print player name" + newPlayer1.PlayerName);
+            Console.WriteLine(PlayerTemplate.PlayerRace(Race));
             Console.ReadKey();
         }
     }
